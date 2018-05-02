78a7de214e0ee1f6c30d601e5d9d760a4f793f0d
wangjinrui <wangjinrui@hongqitech.com>
2018-05-02 23:08:00 +0800

    设置APP进入前台后重置推送角标

--- JRFootballMaze/JRFootballMaze/AppDelegate.m
+++ JRFootballMaze/JRFootballMaze/AppDelegate.m
@@ -83,6 +83,12 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    [application setApplicationIconBadgeNumber:0];
+    // 清空推送角标
+    [JPUSHService resetBadge];
+    // 清除通知栏上的通知
+    [application cancelAllLocalNotifications];
 }
 
 
